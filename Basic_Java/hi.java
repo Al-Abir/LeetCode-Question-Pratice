d8ce0af83ab5af7628c62e5a3b2a49da5e5537bc
Al-Abir <sheikhabir196@gmail.com>
2024-09-18 14:46:13 +0600

    first commit

---
+++ Basic_Java/hi.java
@@ -0,0 +1,7 @@
+package Basic_Java;
+
+public class hi {
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
---
+++ leetCode_15_Pattern/two_pointer.java
@@ -0,0 +1,7 @@
+package leetCode_15_Pattern;
+
+public class two_pointer {
+    public static void main(String[] args) {
+        System.out.println("Two Pointer");
+    }
+}
