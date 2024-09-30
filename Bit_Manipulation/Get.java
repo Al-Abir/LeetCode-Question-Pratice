88f30debbc8f279d39c02efd832573592c8cb4af
Al-Abir <sheikhabir196@gmail.com>
2024-09-30 16:34:18 +0600

    Bit Manipulation

---
+++ Bit_Manipulation/Get.java
@@ -0,0 +1,12 @@
+package Bit_Manipulation;
+
+public class Get {
+    public static void main(String[] args) {
+        int a = 10;
+        int pos =2;
+        int Bit_mask = 1<<pos;
+
+        int result = Bit_mask & a;
+        System.out.println(result);
+    }
+}
