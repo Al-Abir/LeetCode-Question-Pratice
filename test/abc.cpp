861014801094bd41f05e3ce45b4c59bee8c5a045
Al-Abir <sheikhabir196@gmail.com>
2025-07-02 12:47:06 +0600

    Test

---
+++ test/abc.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+#define ll long long
+
+void solve() {
+    // Your code here
+    cout<<"Hello world";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t; // Uncomment for multiple test cases
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
