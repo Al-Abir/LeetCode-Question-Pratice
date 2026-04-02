cb9fc54c21b3a3c7ec2a66b8be4821456e19e637
Al-Abir <sheikhabir196@gmail.com>
2026-04-02 08:35:28 +0600

    Graph

--- Graph/test.cpp
+++ Graph/test.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <list>
+#include<queue>
+#include<vector>
 using namespace std;
 
 class Graph {
@@ -29,7 +31,55 @@ public:
             cout << endl;
         }
     }
+
+    void bfs(){
+        queue<int>q;
+        vector<bool> vis(V,false);
+        q.push(0);
+        vis[0]=true;
+       while (q.size()>0)
+       {
+        /* code */
+        int src = q.front();
+        q.pop();
+
+        cout<<src<<" ";
+
+        for(int neigh: l[src]){
+           if(!vis[neigh]){
+               vis[neigh]=true;
+               q.push(neigh);
+           }
+        }
+       }
+       
+       
+    }
+
+  
+    void dfs(){
+        
+        vector<bool> vis(V,false);
+        for(int i=0;i<V; i++){
+             if(!vis[i]){
+                 dfsHelper(i,vis);
+             }
+        }
+    }
+ void dfsHelper(int src, vector<bool> &vis){
+   
+    cout<<src<<" ";
+    vis[src]=true;
+
+    for(int neigh : l[src]){
+        if(!vis[neigh]){
+            dfsHelper(neigh,vis);
+        }
+    }
+}
 };
+
+
 
 void solve() {
     Graph g(4);
@@ -40,6 +90,9 @@ void solve() {
     g.addEdge(2, 3);
 
     g.PrintEdge();
+    g.bfs();
+     cout<<endl;
+    g.dfs();
 }
 
 int main() {
